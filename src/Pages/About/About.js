00b08e9788dbf3c8e6cd0469907597ad1ec407c3
abdulawalemon1 <emondx4@gmail.com>
2022-04-18 07:39:05 +0600

    about me page added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import emon from "../../images/emon.png";
 const About = () => {
     return (
-        <div>
-            <h2>This is about us</h2>
+        <div className='container d-flex justify-content-start align-items-center'>
+            <div>
+                <img className='' src={emon} alt="" />
+
+            </div>
+            <div>
+                <h2 className='text-center text-primary'>Abdul Awal Emon</h2>
+                <p>Hey, Its Emon here.My goal is to be an efficient Full-stack Developer. I'm going to start my career as a Front-end Developer as I'm pretty confident about my skills on this and will gradually keep working and learning so that I can keep growing as a Developer.I am totally determined for my goals, and I am going to achieve it no matter how hard it is. </p>
+            </div>
         </div>
     );
 };
