5cb4c6d86068b561e3ff4de1138bc52e02eb8ad5
abdulawalemon1 <emondx4@gmail.com>
2022-04-18 19:58:38 +0600

    card design updated

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -20,8 +20,8 @@ const Banner = () => {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3 className='text-light fw-bold'>Welcome to DentaCare</h3>
+                    <p className='text-light fw-bold'>Hi, I'm Dr. Hasnat. Welcome to my Dentacare </p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -32,8 +32,8 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3 className='text-light fw-bold'>Happy Smile</h3>
+                    <p className='text-light fw-bold'>My job is to make your smile beautiful</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -44,9 +44,9 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    <h3 className='text-light fw-bold'>Modern Technology</h3>
+                    <p className='text-light fw-bold'>
+                        Lucrative chamber equiped with modern technologies.
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -11,11 +11,11 @@ const Service = ({ service }) => {
 
 
         <div className="col">
-            <div className="card">
+            <div className="cardBox">
                 <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
-                    <h4 className="card-title">{price}</h4>
+                    <h4 className="card-title">Price: {price}</h4>
                     <p className="card-text">{description}</p>
                     <button onClick={navigateCheckout} className='btn btn-primary'>Book Now</button>
                 </div>
